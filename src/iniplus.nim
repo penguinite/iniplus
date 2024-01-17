9932f78b8ac333d5110cf3d84ac510e580b40b91
penguinite <147942000+penguinite@users.noreply.github.com>
2024-01-17 20:35:10 +0100

    docs: Update iniplus.nim
    Make the CI trigger for once

--- src/iniplus.nim
+++ src/iniplus.nim
@@ -1,3 +1,4 @@
+## This file simply imports and exports the entirety of iniplus.
 import iniplus/[retrieve, objects, reader, writer]
 import std/[tables, times]
 export objects, reader, writer, tables, DateTime, retrieve
