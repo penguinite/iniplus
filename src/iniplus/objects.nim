b9fb80d5722757f1911b7bbaccdd6cb3503c4e27
penguinite <147942000+penguinite@users.noreply.github.com>
2024-01-17 20:43:27 +0100

    docs: Document objects.nim

--- src/iniplus/objects.nim
+++ src/iniplus/objects.nim
@@ -1,14 +1,15 @@
 # Copyright (c) penguinite 2023 <penguinite@proton.me>
 # Licensed under the BSD-3-Clause license
-
+## This module provides the type definitions that are the foundation of this library.
 import std/[tables, strutils]
 export tables
 
 type
+  ## The kind of configuration value we are processing/storing, iniplus only supports Integers, Booleans, Strings and Sequences. (And I guess None, too.)
   ConfigValueKind* = enum
     CVNone, CVInt, CVBool, CVString, CVSequence
 
-
+  ## This object is the actual configuration value. It's best to use the built-in functions when handling these, if you must implement your own logic at the low-level then always remember to check the `kind` field first.
   ConfigValue* = object of RootObj
     case kind*: ConfigValueKind
     of CVNone: nil
@@ -17,10 +18,12 @@ type
     of CVString: stringVal*: string
     of CVSequence: sequenceVal*: seq[ConfigValue]
 
+  ## A "Condensed" configuration value is simply an configuration value with the actual section and key embedded into it. This is used to implement the `setBulkKeys` procedure in the writer module.
   CondensedConfigValue* = object of RootObj
     section*, key*: string
     value*: ConfigValue
 
+  ## Simply a configuration table. The string part follows a format of `SECTION|KEY` so, if you had a key named `port` inside of the section `web` then the format would work out to `web|port`
   ConfigTable* = OrderedTable[string, ConfigValue]
 
 proc isBoolean(raw: string): bool =
@@ -33,6 +36,7 @@ proc isOnlyDigits(raw: string): bool =
   return true
 
 proc trimString*(raw: string): string =
+  ## Trims any double and single quotes from a string.
   if raw == "" or raw == "\"": return ""
   result = raw
   if raw.startsWith('"') or raw.startsWith('\''): result = result[1..^1]
@@ -78,6 +82,7 @@ proc trimArrayString(raw: string): string =
   return result
 
 proc convertValue*(raw: string): ConfigValue =
+  ## Converts a raw string into a configuration value, this is primarily used by the parser.
   let kind = getKind(raw)
   result = ConfigValue(kind: kind)
 
