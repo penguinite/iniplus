929ba5db29ae4777d758f4e2f79ddf3e915c42ef
penguinite <penguinite@tuta.io>
2024-08-06 15:12:17 +0200

    feat: New config parser
    This is what makes the table magic possible.
    
    It doesn't support nested tables or tables with arrays inside but it's alright.

--- src/iniplus/reader.nim
+++ src/iniplus/reader.nim
@@ -8,16 +8,9 @@ runnableExamples "--run:off":
 runnableExamples:
   discard parseString("key=\"value\"")
 
-import objects, strutils, tables
+import objects, strutils
+import private/utils
 export objects
-
-type
-  ConfigParserMode = enum
-    None, Section, PreValue, Single, Multi
-
-
-template log(str: varargs[string,`$`]) =
-  echo("[iniplus:" & instantiationInfo().filename & ":" & $(instantiationInfo().line) & "]: " & str.join())
 
 proc parseString*(input: string): ConfigTable =
   ## This procedure takes a string of some kind as its input and returns a parsed ConfigTable object.
@@ -25,83 +18,143 @@ proc parseString*(input: string): ConfigTable =
     import iniplus
     let config = parseString("my_favorite_key=\"My Favorite Value\"\n")
     assert config.getString("","my_favorite_key") == "My Favorite Value"
+
   var
-    section, key, value = ""
-    mode: ConfigParserMode = None
-    inQuote, backslash, forceInsert = false
+    quoted, backSlash = false
+    tokens: seq[Token] = @[]
+    tmp = ""
 
-  for line in input.splitLines:
-    if line.isEmptyOrWhitespace() or line.startsWith("#") or line.startsWith(";") and mode != Multi:
-      continue # Skip since line is either comment or mostly empty
-    
-    if forceInsert or mode == Single:
-      # Clean the key and value beforehand so the code is slightly prettier
-      key = strip(key)
-      value = strip(value)
-      result[section & '|' & key] = convertValue(value)
-      key = ""
-      value = ""
-      inQuote = false
-      mode = None
-      forceInsert = false
+  proc add(k: TokenKind, i = "") =
+    case k:
+    of Literal, Quoted:
+      if not tmp.isEmptyOrWhitespace():
+        tokens.add(Token(kind: k, inner: strip(i)))
+      tmp = ""
+    else:
+      if len(tmp) > 0:
+        if quoted: add Quoted, tmp
+        else: add Literal, tmp
+        tmp = ""
+      tokens.add(Token(kind: k))
 
-    for ch in line:
-      #echo "[$#, $#, $#]" % [$ch, $mode, value]
-      if ch == '\\': backslash = true
-
-      if ch == '"' and not backslash:
-        if inQuote: inQuote = false
-        else: inQuote = true
-      
-      if backslash: backslash = false
-
-      case mode:
-      of None:
-        if not inQuote:
-          if ch == '[':
-            section = ""
-            mode = Section
-            continue
-          
-          if ch == '=' or ch == ':':
-            mode = PreValue
-            key = key.trimString()
-            continue
-        key.add(ch)
-
-      of Section:
-        if ch == ']' and not inQuote:
-          mode = None
-          section = section.strip()
+  for ch in input:
+    # Deal with quotes right off the bat
+    if quoted:
+      case ch:
+      of '\\':
+        # Pretty basic backslash handling.
+        # If there are 2 backslashes in a row, then just insert a backslash into tmp.
+        # Otherwise, flip the backslash boolean to true.
+        if backslash:
+          tmp.add('\\')
+          backslash = false
+        else:
+          backslash = true
+      of '"':
+        # If there has been a backslash, then add a double quote into tmp
+        # And disable backslash
+        if backSlash:
+          tmp.add('"')
+          backSlash = false
           continue
 
-        if ch == '|':
-          log "| cannot be used inside sections! Replacing with _"            
-          section.add("_")
-          continue
+        # Otherwise, disable quote flag.
+        if len(tmp) > 0:
+          add Quoted, tmp
+        quoted = false
+      else:
+        tmp.add(ch)
+      continue
 
-        section.add(ch)
-      of PreValue:
-        if ch in Whitespace: continue
-        
-        if not inQuote: value.add(ch)
-        
-        if ch == '[': mode = Multi
-        else: mode = Single
-      of Single:
-        value.add(ch)
-      of Multi:
-        if ch == ']' and not inQuote:
-          mode = Single
-          forceInsert = true
-        value.add(ch)
-  
-  if forceInsert or mode == Single:
-    # Clean the key and value beforehand so the code is slightly prettier
-    key = strip(key)
-    value = strip(value)
-    result[section & '|' & key] = convertValue(value)
-  
+    case ch:
+    of '"':
+      # If we see a double quote then turn on the quote flag
+      if len(tmp) > 0:
+        add Literal, tmp
+      quoted = true
+    # The above quote handling stuff means we can just
+    # call add to add these and nothing else.
+    # How clean...
+    of '[': add SquareOpen
+    of ']': add SquareClose
+    of '=': add EqualSign
+    of '{': add CurlyOpen
+    of '}': add CurlyClose
+    of ':': add Colon
+    of ',': add Comma
+    of '\n': add Literal, tmp
+    else: tmp.add(ch)
+
+  # One last check
+  # To see if tmp is empty or not
+  if quoted: add Quoted, tmp
+  else: add Literal, tmp
+
+  var
+    state = None
+    section, key = ""
+    tmpSeq: seq[string]
+
+  for token in tokens:
+    case token.kind:
+    of SquareOpen:
+      # If we haven't yet seen an equal sign then
+      # this is most likely a section.
+      case state:
+      of None: state = Section 
+      of Val: state = Array
+      else: discard
+
+    of SquareClose:
+      # If we are currently parsing an array then
+      # y'know, parse an array... Otherwise assume it's a section
+      # and just close the section.
+      case state:
+      of Section: state = None
+      of Array:
+        state = None
+        result[(section, key)] = conv(tmpSeq)
+        tmpSeq = @[]
+      else: discard
+
+    of EqualSign:
+      # If we haven't yet seen anything and we have already added a key
+      # then just start parsing the next tokens as values
+      if state == None and len(key) > 0:
+        state = Val
+
+    of CurlyOpen:
+      # If we are parsing values then just start parsing the next tokens
+      # as a table.
+      if state == Val:
+        state = CTable
+
+    of CurlyClose:
+      # If we're parsing tables then y'know, parse the table and add it to the result
+      if state == CTable:
+        state = None
+        result[(section, key)] = conv(tmpSeq, true)
+        tmpSeq = @[]
+
+    of Literal, Quoted:
+      # Do a range of stuff depending on our current state
+      case state:
+      # Just copy it as the section
+      of Section: section.add(token.inner)
+      # If we aren't parsing anything then use it as the key
+      of None: key = token.inner
+      # If we are parsing something then use it as the value
+      # and add it to the table.
+      # But DO keep the quotes. They'll be trimmed later anyway
+      of Val:
+        if token.kind == Quoted:
+          result[(section, key)] = conv("\"" & token.inner & "\"")
+        else:
+          result[(section, key)] = conv(token.inner)
+        state = None
+      # If we are parsing arrays/tables then just add it to the tmpseq where it will be handled later
+      of Array, CTable: tmpSeq.add(token.inner)
+    else: discard # We don't use comma or colon, but y'know, we parse them so they are kept out of the Literal and Quotes tokens.
   
 proc parseFile*(filename: string): ConfigTable =
   ## Opens a file, reads the entirety of it and returns a configuration table.
