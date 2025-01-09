5a474219fccd9c98cfe454c6d00950b74d9d595e
penguinite <penguinite@tuta.io>
2025-01-09 13:14:12 +0100

    refactor: Port test3 to std/unittest

--- tests/test3.nim
+++ tests/test3.nim
@@ -1,10 +1,11 @@
-import iniplus
+import iniplus, std/unittest
 
 let conf = parseString("""
 I_like_to = live # dangerously
 I_love_to = "live # dangerously"
 
-Id_like_to = "live "#dangerously
+Id_like_to = live#dangerously
+Id_love_to = "live "#dangerously
 #Id_hate_to = live # safely
 
 ###aa=aa
@@ -15,15 +16,37 @@ Id_like_to = "live "#dangerously
 
 """)
 
-assert conf.getString("","I_like_to") == "live"
-assert conf.getString("","I_love_to") == "live # dangerously"
-assert conf.getString("","Id_like_to") == "live "
+suite "Strange syntax":
+  test "Inline comment":
+    assert conf.getString("","I_like_to") == "live"
 
+  test "Inline comment quoted":
+    assert conf.getString("","I_love_to") == "live # dangerously"
+  
+  test "Inline comment compact":
+    assert conf.getString("","Id_like_to") == "live"
+  
+  test "Inline comment compact quoted":
+    assert conf.getString("","Id_love_to") == "live "
+  
+  test "Inline comment in a comment":
+    assert conf.exists("","Id_hate_to") != true
+  
+  test "Triple commented line":
+    assert conf.exists("","aa") != true
+  
+  test "Double commented line":
+    assert conf.exists("","f") != true
 
-assert conf.exists("","Id_hate_to") != true
-assert conf.exists("","aa") != true
-assert conf.exists("","f") != true
-assert conf.exists("","d") != true
-assert conf.exists("","b") != true
-assert conf.exists("","a") != true
-assert conf.exists("","c") != true
+  test "Double commented line #2":
+    assert conf.exists("","d") != true
+
+  test "Single comment line":
+    assert conf.exists("","a") != true
+
+  test "Single comment line #2":
+    assert conf.exists("","b") != true
+  
+  test "Nonexistent key-val pair":
+    assert conf.exists("","c") != true
+  
