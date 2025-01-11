f59709f5a312c274354c2211941785d9ebe07bfd
penguinite <penguinite@tuta.io>
2025-01-11 03:57:21 +0100

    refactor: Remove special retrieve funcs and embed them into the normal retrieve.nim file

--- src/iniplus/retrieve.nim
+++ src/iniplus/retrieve.nim
@@ -30,7 +30,14 @@ func exists*(table: ConfigTable, section, key: string): bool =
     
     # This isn't in the config file, so it's false.
     assert config.exists("","age") == false
-  return table.hasKey((section, key))
+  
+  when defined(iniplusCheckmaps):
+    # Checks have already been done when the table was parsed.
+    # And if the key doesn't exist, then it's the application's fault
+    # for not using the feature properly...
+    return true 
+  else:
+    return table.hasKey((section, key))
 
 func getValue*(table: ConfigTable, section, key: string): ConfigValue =
   ## Returns a pure ConfigValue object, typically this is only used for low-level retrieval.
@@ -40,9 +47,12 @@ func getValue*(table: ConfigTable, section, key: string): ConfigValue =
     assert config.getValue("","name").kind == CVString
     assert config.getValue("","name").stringVal == "John Doe"
 
-  if not table.hasKey((section, key)):
-    raiseIndexDefect(section, key)
-  return table[(section, key)]
+  when defined(iniplusCheckmaps):
+    return table[(section, key)]
+  else:
+    if not table.exists(section, key):
+      raiseIndexDefect(section, key)
+    return table[(section, key)]
 
 func getString*(table: ConfigTable, section, key: string): string =
   ## Returns a string from a table with the specified section and key.
@@ -53,12 +63,14 @@ func getString*(table: ConfigTable, section, key: string): string =
     info_text = "Insert some informational text here."
     """)
     assert config.getString("dialog","info_text") == "Insert some informational text here."
-  if not table.hasKey((section, key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  if val.kind != CVString:
-    raiseValueError(val.kind, section, key)
-  return val.stringVal
+
+  when defined(iniplusCheckmaps):
+    return table[(section, key)].stringVal
+  else:
+    let val = table.getValue(section, key)
+    if val.kind != CVString:
+      raiseValueError(val.kind, section, key)
+    return val.stringVal
 
 func getStringOrDefault*(table: ConfigTable, section, key, default: string): string =
   ## Returns a string from a table with the specified section and key, *or* if the key does
@@ -74,12 +86,16 @@ func getStringOrDefault*(table: ConfigTable, section, key, default: string): str
     # This is not in the config file, so the procedure returns the `default` parameter.
     assert config.getStringOrDefault("dialog","help","Helpful text") == "Helpful text"
 
-  if table.hasKey((section, key)):
-    let val = table[(section, key)]
-    if val.kind != CVString:
-      raiseValueError(val.kind, section, key)
-    return val.stringVal
-  return default
+  when defined(iniplusCheckmaps):
+    # If the application uses checkmaps properly
+    # then we can just pass the key as-is
+    # if the key didn't exist when parsing
+    # then iniplus would have added used its default value.
+    return table[(section, key)].stringVal
+  else:
+    if table.exists(section, key):
+      return table.getString(section, key)
+    return default
 
 func getBool*(table: ConfigTable, section, key: string): bool =
   ## Returns a boolean from a table with the specified section and key.
@@ -87,13 +103,15 @@ func getBool*(table: ConfigTable, section, key: string): bool =
     import iniplus
     let config = parseString("enable_feature = true")
     assert config.getBool("","enable_feature") == true
-  if not table.hasKey((section, key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  case val.kind:
-  of CVString: result = parseBool(val.stringVal)
-  of CVBool: result = val.boolVal
-  else: raiseValueError(val.kind, section, key)
+  
+  when defined(iniplusCheckmaps):
+    return table[(section, key)].boolVal
+  else:
+    let val = table.getValue(section, key)
+    case val.kind:
+    of CVString: return parseBool(val.stringVal)
+    of CVBool: return val.boolVal
+    else: raiseValueError(val.kind, section, key)
 
 func getBoolOrDefault*(table: ConfigTable, section, key: string, default: bool): bool =
   ## Either returns the provided boolean in a table or a default value.
@@ -107,13 +125,12 @@ func getBoolOrDefault*(table: ConfigTable, section, key: string, default: bool):
     assert table2.getBoolOrDefault("", "enabled", false) == false
     assert table2.getBoolOrDefault("", "enabled", true) == true
   
-  if table.hasKey((section, key)):
-    let val = table[(section, key)]
-    case val.kind:
-    of CVString: return parseBool(val.stringVal)
-    of CVBool: return val.boolVal
-    else: raiseValueError(val.kind, section, key)
-  return default
+  when defined(iniplusCheckmaps):
+    return table[(section, key)].boolVal
+  else:
+    if table.exists(section, key):
+      return table.getBool(section, key)
+    return default
 
 func getInt*(table: ConfigTable, section, key: string): int =
   ## Returns an integer from a table with the specified section and key.
@@ -122,13 +139,14 @@ func getInt*(table: ConfigTable, section, key: string): int =
     let config = parseString("port = 8080")
     assert config.getInt("","port") == 8080
 
-  if not table.hasKey((section, key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  case val.kind:
-  of CVString: return parseInt(val.stringVal)
-  of CVInt: return val.intVal
-  else: raiseValueError(val.kind, section, key)
+  when defined(iniplusCheckmaps):
+    return table[(section, key)].intVal
+  else:
+    let val = table.getValue(section, key)
+    case val.kind:
+    of CVString: return parseInt(val.stringVal)
+    of CVInt: return val.intVal
+    else: raiseValueError(val.kind, section, key)
 
 func getIntOrDefault*(table: ConfigTable, section, key: string, default: int): int =
   ## Either returns the provided integer in a table or a default value.
@@ -141,13 +159,12 @@ func getIntOrDefault*(table: ConfigTable, section, key: string, default: int): i
     let table2 = parseString("")
     assert table2.getIntOrDefault("", "port", 1000) == 1000
     assert table2.getIntOrDefault("", "port", 1010) == 1010
-  if table.hasKey((section, key)):
-    let val = table[(section,key)]
-    case val.kind:
-    of CVString: return parseInt(val.stringVal)
-    of CVInt: return val.intVal
-    else: raiseValueError(val.kind, section, key)
-  return default
+  when defined(iniplusCheckmaps):
+    return table[(section, key)].intVal
+  else:
+    if table.exists(section, key):
+      return table.getInt(section, key)
+    return default
 
 func getArray*(table: ConfigTable, section, key: string): seq[ConfigValue] =
   ## Returns an array containing a set of ConfigValue objects from a table with the specified section and key.
@@ -164,12 +181,14 @@ func getArray*(table: ConfigTable, section, key: string): seq[ConfigValue] =
     assert employees[0].stringVal == "John"
     assert employees[1].stringVal == "Katie"
     assert employees[2].intVal == 1000
-  if not table.hasKey((section,key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  if val.kind != CVArray:
-    raiseValueError(val.kind, section, key)
-  return val.arrayVal
+  
+  when defined(iniplusCheckmaps):
+    return table[(section, key)].arrayVal
+  else:
+    let val = table.getValue(section, key)
+    if val.kind != CVArray:
+      raiseValueError(val.kind, section, key)
+    return val.arrayVal
 
 func getStringArray*(table: ConfigTable, section, key: string): seq[string] =
   ## This procedure retrieves a string-only array from a table. It also throws out any non-string items
@@ -182,14 +201,19 @@ func getStringArray*(table: ConfigTable, section, key: string): seq[string] =
     assert employees[0] == "John"
     assert employees[1] == "Katie"
     assert len(employees) == 2
-  if not table.hasKey((section,key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  if val.kind != CVArray:
-    raiseValueError(val.kind, section, key)
-  for item in val.arrayVal:
-    if item.kind == CVString: result.add(item.stringVal)
-  return result
+
+  when defined(iniplusCheckmaps):
+    for i in table[(section, key)].arrayVal:
+      case i.kind:
+      of CVString: result.add(i.stringVal)
+      else: discard
+    return result
+  else:
+    for i in table.getArray(section, key):
+      case i.kind:
+      of CVString: result.add(i.stringVal)
+      else: discard
+    return result
 
 func getStringArrayOrDefault*(table: ConfigTable, section, key: string, default: seq[string]): seq[string] =
   ## Either returns the provided string array in a table or a default value.
@@ -202,7 +226,12 @@ func getStringArrayOrDefault*(table: ConfigTable, section, key: string, default:
     let table2 = parseString("")
     assert table2.getStringArrayOrDefault("", "users", @["John"]) == @["John"]
     assert table2.getStringArrayOrDefault("", "users", @[]) == @[]
-  if not table.hasKey((section,key)):
+  
+  when defined(iniplusCheckmaps):
+    return table.getStringArray(section, key)
+  else:
+    if table.exists(section, key):
+      return table.getStringArray(section, key)
     return default
 
 func getIntArray*(table: ConfigTable, section, key: string): seq[int] =
@@ -216,14 +245,18 @@ func getIntArray*(table: ConfigTable, section, key: string): seq[int] =
     assert number[0] == 1000
     assert number[1] == 2000
     assert len(number) == 2
-  if not table.hasKey((section, key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  if val.kind != CVArray:
-    raiseValueError(val.kind, section, key)
-  for item in val.arrayVal:
-    if item.kind == CVInt: result.add(item.intVal)
-  return result
+  when defined(iniplusCheckmaps):
+    for i in table[(section, key)].arrayVal:
+      case i.kind:
+      of CVInt: result.add(i.intVal)
+      else: discard
+    return result
+  else:
+    for i in table.getArray(section, key):
+      case i.kind:
+      of CVInt: result.add(i.intVal)
+      else: discard
+    return result
 
 func getIntArrayOrDefault*(table: ConfigTable, section, key: string, default: seq[int]): seq[int] =
   ## Either returns the provided int array in a table or a default value.
@@ -239,12 +272,12 @@ func getIntArrayOrDefault*(table: ConfigTable, section, key: string, default: se
   if not table.hasKey((section,key)):
     return default
 
-  let val = table[(section, key)]
-  if val.kind != CVArray:
-    raiseValueError(val.kind, section, key)
-  for item in val.arrayVal:
-    if item.kind == CVInt: result.add(item.intVal)
-  return result
+  when defined(iniplusCheckmaps):
+    return table.getIntArray(section, key)
+  else:
+    if table.exists(section, key):
+      return table.getIntArray(section, key)
+    return default
 
 func getBoolArray*(table: ConfigTable, section, key: string): seq[bool] =
   ## This procedure retrieves a boolean-only array from a table. It also throws out any non-boolean items
@@ -257,14 +290,18 @@ func getBoolArray*(table: ConfigTable, section, key: string): seq[bool] =
     assert myFavoriteBooleans[0] == true
     assert myFavoriteBooleans[1] == false
     assert len(myFavoriteBooleans) == 2
-  if not table.hasKey((section,key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  if val.kind != CVArray:
-    raiseValueError(val.kind, section, key)
-  for item in val.arrayVal:
-    if item.kind == CVBool: result.add(item.boolVal)
-  return result
+  when defined(iniplusCheckmaps):
+    for i in table[(section, key)].arrayVal:
+      case i.kind:
+      of CVBool: result.add(i.boolVal)
+      else: discard
+    return result
+  else:
+    for i in table.getArray(section,key):
+      case i.kind:
+      of CVBool: result.add(i.boolVal)
+      else: discard
+    return result
 
 func getBoolArrayOrDefault*(table: ConfigTable, section, key: string, default: seq[bool]): seq[bool] =
   ## Either returns the provided bool array in a table or a default value.
@@ -275,47 +312,44 @@ func getBoolArrayOrDefault*(table: ConfigTable, section, key: string, default: s
     assert table.getStringArrayOrDefault("", "users", @[]) == @["Kate", "John", "Alex"]
 
     let table2 = parseString("")
-    assert table2.getStringArrayOrDefault("", "users", @["John"]) == @["John"]
-    assert table2.getStringArrayOrDefault("", "users", @[]) == @[]
-  if not table.hasKey((section,key)):
+  when defined(iniplusCheckmaps):
+    return table.getBoolArray(section, key)
+  else:
+    if table.exists(section, key):
+      return table.getBoolArray(section, key)
     return default
 
-  let val = table[(section, key)]
-  if val.kind != CVArray:
-    raiseValueError(val.kind, section, key)
-  for item in val.arrayVal:
-    if item.kind == CVBool: result.add(item.boolVal)
-  return result
-
-proc getTable*(table: ConfigTable, section, key: string): OrderedTable[string, ConfigValue] =
+func getTable*(table: ConfigTable, section, key: string): OrderedTable[string, ConfigValue] =
   ## Returns a table from a configuration table with the specified section and key.
   runnableExamples:
     import iniplus
     let config = parseString("names_and_age = {\"John\": 21, \"Kate\": 22}")
     assert config.getTable("","names_and_age").len() == 2
-  if not table.hasKey((section, key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  if val.kind != CVTable:
-    raiseValueError(val.kind, section, key)
-  return val.tableVal
+  when defined(iniplusCheckmaps):
+    return table[(section, key)].tableVal
+  else:
+    let val = table.getValue(section, key)
+    if val.kind != CVTable:
+      raiseValueError(val.kind, section, key)
+    return val.tableVal
 
 proc getStringTable*(table: ConfigTable, section, key: string): OrderedTable[string, string] =
   ## Returns a string-only table from a configuration table with the specified section and key.
   runnableExamples:
     import iniplus
     let config = parseString("names_and_likes = {\"John\": \"Dogs\", \"Kate\": \"Cats\"}")
-    assert config.getTable("","names_and_likes").len() == 2
-  if not table.hasKey((section, key)):
-    raiseIndexDefect(section, key)
-  let val = table[(section, key)]
-  if val.kind != CVTable:
-    raiseValueError(val.kind, section, key)
-  
-  for key,val2 in val.tableVal.pairs:
-    if val2.kind == CVString:
-      result[key] = val2.stringVal
-  return result
+  when defined(iniplusCheckmaps):
+    for key,val in table[(section, key)].tableVal.pairs:
+      case val.kind:
+      of CVString: result[key] = val.stringVal
+      else: discard
+    return result
+  else:
+    for key,val in table.getTable(section, key).pairs:
+      case val.kind:
+      of CVString: result[key] = val.stringVal
+      else: discard
+    return result
 
 proc getBoolTable*(table: ConfigTable, section, key: string): OrderedTable[string, bool] =
   ## Returns a boolean-only table from a configuration table with the specified section and key.
@@ -327,10 +361,18 @@ proc getBoolTable*(table: ConfigTable, section, key: string): OrderedTable[strin
   if val.kind != CVTable:
     raiseValueError(val.kind, section, key)
   
-  for key,val2 in val.tableVal.pairs:
-    if val2.kind == CVBool:
-      result[key] = val2.boolVal
-  return result
+  when defined(iniplusCheckmaps):
+    for key,val in table[(section, key)].tableVal.pairs:
+      case val.kind:
+      of CVBool: result[key] = val.boolVal
+      else: discard
+    return result
+  else:
+    for key,val in table.getTable(section, key).pairs:
+      case val.kind:
+      of CVBool: result[key] = val.boolVal
+      else: discard
+    return result
 
 proc getIntTable*(table: ConfigTable, section, key: string): OrderedTable[string, int] =
   ## Returns a integer-only table from a configuration table with the specified section and key.
@@ -342,7 +384,15 @@ proc getIntTable*(table: ConfigTable, section, key: string): OrderedTable[string
   if val.kind != CVTable:
     raiseValueError(val.kind, section, key)
   
-  for key,val2 in val.tableVal.pairs:
-    if val2.kind == CVInt:
-      result[key] = val2.intVal
-  return result
+  when defined(iniplusCheckmaps):
+    for key,val in table[(section, key)].tableVal.pairs:
+      case val.kind:
+      of CVInt: result[key] = val.intVal
+      else: discard
+    return result
+  else:
+    for key,val in table.getTable(section, key).pairs:
+      case val.kind:
+      of CVInt: result[key] = val.intVal
+      else: discard
+    return result
